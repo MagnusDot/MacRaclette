8516a56905b60eff0ca95f4f85dcf23154e5c1e7
Magnus <a.marcellin@reply.com>
2026-06-10 14:04:15 +0200

    Initial Commit

---
+++ MacRacletteTests/MacRacletteTests.swift
@@ -0,0 +1,19 @@
+//
+//  MacRacletteTests.swift
+//  MacRacletteTests
+//
+//  Created by Alois Marcellin on 10/06/2026.
+//
+
+import Testing
+@testable import MacRaclette
+
+struct MacRacletteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
---
+++ MacRacletteUITests/MacRacletteUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  MacRacletteUITestsLaunchTests.swift
+//  MacRacletteUITests
+//
+//  Created by Alois Marcellin on 10/06/2026.
+//
+
+import XCTest
+
+final class MacRacletteUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
